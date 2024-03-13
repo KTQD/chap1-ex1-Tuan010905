dc5f0c37a344c6615b27480a785997039c68d204
Tuan010905 <163240092+Tuan010905@users.noreply.github.com>
2024-03-13 14:20:27 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 
-int main() {
-    //Write your code here
+int main()
+{
+    printf("National Economics University");
+
     return 0;
 }
